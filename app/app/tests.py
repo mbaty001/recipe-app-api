2b6b69a7b088bc670cfa3dbbf380f89d74442987
Michal Batyra <michal_batyra@epam.com>
2023-08-29 13:00:44 +0200

    Simple tests and add postgresql

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add(self):
+
+        self.assertEqual(calc.add(5, 6), 11)
+
+    def test_subtract(self):
+        
+        self.assertAlmostEqual(calc.substract(10, 15), 5)
