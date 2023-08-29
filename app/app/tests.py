cf22f1bbc6d7eaf9d34cddeefd5a4f3a91660a6a
Michal Batyra <michal_batyra@epam.com>
2023-08-29 13:08:44 +0200

    Correct lints

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add(self):
@@ -9,5 +10,5 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(calc.add(5, 6), 11)
 
     def test_subtract(self):
-        
+
         self.assertAlmostEqual(calc.substract(10, 15), 5)
