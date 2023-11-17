d5e8fb822d754356c6a89817318a3c17dda1b96e
Michal Batyra <michal_batyra@epam.com>
2023-11-17 14:27:48 +0100

    lints

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -15,9 +15,11 @@ from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
 
+
 def detail_url(tag_id):
     """Create and return a tag detail url."""
     return reverse('recipe:tag-detail', args=[tag_id])
+
 
 def create_user(email='user@example.com', password="testpass123"):
     """Create and return a user."""
